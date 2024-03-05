f899b923cebcbbd8a83c20f2652eb91bdca73fb1
Yuvneesh Kashyap <yuvneeshk53@gmail.com>
2024-03-05 17:47:32 -0500

    Demonstarted use of pydoc in a simple script with no functions

---
+++ simple_script/script_1.md
@@ -0,0 +1,18 @@
+<a id="script_1"></a>
+
+# script\_1
+
+<a id="script_1.name"></a>
+
+#### name
+
+This is a special comment.
+We can use this to tell us about the variable in use i.e. name
+
+<a id="script_1.last_name"></a>
+
+#### last\_name
+
+Alternatively, we can add as many lines of comment we want.
+It will work as long as the last line has "#:" symbol.
+
---
+++ simple_script/script_1.py
@@ -0,0 +1,17 @@
+import os
+
+# This is just a simple comment before a variable.
+# This won't be registered in documentation
+variable_without_docstring = "Hi, World"
+
+#: This is a special comment.
+#: We can use this to tell us about the variable in use i.e. name
+name = "Yuvneesh"
+
+# Alternatively, we can add as many lines of comment we want.
+# It will work as long as the last line has "#:" symbol.
+#:
+last_name = "Kashyap"
+
+#: Without a variable, this won't do anything.
+print(name + last_name)
